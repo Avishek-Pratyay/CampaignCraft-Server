ddd6f47104d339931a738c1c10de6c4c94aebc81
Avishek-Pratyay <avishekp.aiub@gmail.com>
2026-07-18 17:28:38 +0600

    nn

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://campaign-craft-client-kappa.vercel.app'],
+  origin: ['https://campaign-craft-client-kappa.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
